92d14f950469b1b429addda0aa565c14636195d0
li,xiaokun <leeli@iu.edu>
2023-06-23 15:34:43 -0400

    set up transaction date and drop down

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -29,7 +29,8 @@ namespace Expense_Tracker.Controllers
         // GET: Transaction/AddOrEdit
         public IActionResult AddOrEdit()
         {
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
+            // ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
+            PopulateCategories();
             return View(new Transaction());
         }
 
@@ -67,6 +68,15 @@ namespace Expense_Tracker.Controllers
             
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        [NonAction]
+        public void PopulateCategories()
+        {
+            var CategoryCollection = _context.Categories.ToList();
+            Category DefaultCategory = new Category() {CategoryId = 0, Title = "Choose a Category"};
+            CategoryCollection.Insert(0, DefaultCategory);
+            ViewBag.Categories = CategoryCollection;
         }
 
     }
