6885968a085913f659e0f3b659809abf4f50ff15
li,xiaokun <leeli@iu.edu>
2023-06-25 09:21:19 -0400

    added dash board controller

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,32 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
-namespace Expense_Tracker.Controllers
+namespace Expense_Tracker.Controllers;
+
+public class DashboardController : Controller
 {
-    [Route("[controller]")]
-    public class DashboardController : Controller
+    public IActionResult Index()
     {
-        private readonly ILogger<DashboardController> _logger;
-
-        public DashboardController(ILogger<DashboardController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
-        }
+        return View();
     }
+
 }
